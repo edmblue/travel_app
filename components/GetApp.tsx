38c0f0e82796c81372b90bd20fd3c1ffeb6f3977
Raquel Betancourt <brsarayb@gmail.com>
2024-04-22 10:28:38 -0300

    getapp section start

---
+++ components/GetApp.tsx
@@ -0,0 +1,14 @@
+const GetApp = () => {
+  return (
+    <section className="max-container padding-container bg-center bg-cover bg-green-90 lg:rounded-5xl bg-pattern py-10">
+      <div className="flex flex-col gap-3">
+        <h2 className="text-white bold-40 lg:bold-64">Get for free now!</h2>
+        <p className="regular-16 lg:regular-18 text-gray-10">
+          Available on iOS and Android
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
